e4dc364a684a3779106f49cb8ea59f255c97f297
Joshua <fehrenbachjoshua3@gmail.com>
2019-09-10 12:58:19 -0400

    Unpack WebContent and Add LoginHandler class
    Un-Zipped the WebContent archive (since there's not much of a point in it being archived) and added an initial outline of the class LoginHandler. The class doesn't actually do anything right now since I don't know anything about the database layout, but the initial outline is there.

---
+++ WebContent/Test.jsp
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="test.css">
+    <title>Small Town Battleships</title>
+  </head>
+  <body>
+	<div class="login">
+  		<form action="/action_page.php" class="container">
+   			<label for="username">Username</label>
+   			<br>
+    		<input type="text" placeholder="Enter Email" name="username" required>
+    		<br>
+    		<label for="psw">Password</label>
+    		<br>
+    		<input type="password" placeholder="Enter Password" name="psw" required>
+    		<br>
+    		<button type="submit" class="btn">Login</button>
+  		</form>
+  	</div>
+  </body>
+</html>
---
+++ src/me/smalltownships/LoginHandler.java
@@ -0,0 +1,71 @@
+package me.smalltownships;
+
+public class LoginHandler {
+	
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Cannot find JDBC libraries", e);
+		}
+	}
+
+	// FIXME: Need to know the database layout to write the queries
+	
+	private MySQLHandler sqlHandler;
+	
+	public LoginHandler() {
+		sqlHandler = new MySQLHandler();
+	}
+	
+	/**
+	 * Attempt to login using the given username and password
+	 * 
+	 * @param user The username to search for
+	 * @param password The password to search for
+	 * @return True if the username and password match, false otherwise
+	 */
+	public boolean tryLogin(String user, String password) {
+		// TODO: Query database for an account with the given username and password
+		// TODO: If the username and password match an account, set the login flag
+		return true;
+	}
+	
+	/**
+	 * Check whether an account with the given email exists.
+	 * 
+	 * @param email The email address to check for
+	 * @return True if an account has the email address "email"
+	 */
+	public boolean emailExists(String email) {
+		// TODO: Query the database for an account with the given email address
+		return false;
+	}
+	
+	/**
+	 * Check whether an account with the given username exists.
+	 * <p>
+	 * May be redundant if the user's email address is used as their username.
+	 * 
+	 * @param user The username to check for
+	 * @return True if an account has the username "user"
+	 */
+	public boolean usernameExists(String user) {
+		// TODO: Query the database for an account with the given username
+		return false;
+	}
+	
+	/**
+	 * Attempts to create a new account on the website.
+	 * 
+	 * @param user The username of the new account
+	 * @param password The password of the new account
+	 * @param email The email address of the new user (redundant if username is email address)
+	 * @return True if a new account was created, false otherwise
+	 */
+	public boolean createNewUser(String user, String password, String email) {
+		// TODO: Check if an account with the given username and/or password exists
+		// TODO: Add a new account to the DB with the given username, password and email address
+		return false;
+	}
+}
