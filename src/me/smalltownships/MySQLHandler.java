8bc49c039b66db8a8671ab59ae5a7c58a3d740a0
Joshua <fehrenbachjoshua3@gmail.com>
2019-09-18 11:38:10 -0400

    Fix accidental changes to MySQLHandler
    My last push went kinda weird, so this is fixing that

--- src/me/smalltownships/MySQLHandler.java
+++ src/me/smalltownships/MySQLHandler.java
@@ -15,7 +15,13 @@ public class MySQLHandler implements AutoCloseable {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 *  Designed to be called by LoginHandler / Registration page
+	 *  Returns a result set that can be manipulated
+	 *  
+	 *  TODO Secure this via 'protected' in package with classes that need access to it
+	 */
 	public ResultSet performStatement(String sql) {
 		ResultSet rs = null;
 		try {
@@ -25,6 +31,28 @@ public class MySQLHandler implements AutoCloseable {
 			e.printStackTrace();
 		}
 		return rs;
+	}
+	
+	/**
+	 * Utility method, allowing for conversion of verifiedAccounts table to String
+	 */
+	public String verifiedToString(ResultSet rs) {
+		String s = "";
+		try {
+			int columnCount = rs.getMetaData().getColumnCount();
+			while(rs.next()){
+				s = s.concat(rs.getRow()+": ");
+				for (int i = 1; i <= columnCount; i++) {
+					String value = rs.getString(i);
+					s = s.concat("["+value+"] ");
+				}
+				s = s.concat("\n");
+			}
+			return s;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	@Override
