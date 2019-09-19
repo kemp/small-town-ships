59cf16a987a52ff069f2e46d2077299712a49e4a
qwertyuiop546 <adrian.muan@yahoo.com>
2019-09-19 16:10:32 -0400

    update

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -4,13 +4,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<jsp::include page="/WEB-INF/views/_head.jsp">
+</jsp::include>
 <link rel="stylesheet" href="index.css">
 <title>Small Town Ships</title>
 </head>
 <body>
-	<form action="Controller" method="POST">
+	<form action="LoginServlet" method="POST">
 		<h1>Login</h1>
 	 	<div class="form-group">
 	    	<label for="username">Username</label>
---
+++ src/me/smalltownships/LoginServlet.java
@@ -0,0 +1,69 @@
+package me.smalltownships;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		try
+		{
+			if(username == null)
+			{
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/smalltownships","root","123456789");
+				Statement query = conn.createStatement();
+				ResultSet rs = query.executeQuery("select username, password from verified accounts");
+				
+				if(rs.next())
+				{
+					response.sendRedirect("Products.jsp");
+				}
+				else
+				{
+					System.out.println("Login Failed");
+				}
+				
+				conn.close();
+			}
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Exception" + ex.getMessage());
+		}
+		
+	}
+
+}
