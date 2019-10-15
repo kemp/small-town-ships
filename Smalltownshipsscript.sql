0d356a8a3b3012abd04c842f02a201fe945bbca3
Noah Huck <nhuck20@gmail.com>
2019-10-15 19:04:56 -0400

    New Database script / Design
    The verified account table now has a permissions column, and I thnik 0 should be a standard user, acnd 1 should be an admin.
    An inventory table has been added.
    To handle transactions, the 'transactions' table will hold price and personal information about a transaction, the 'ims' table will hold +- quantity information about particular items.
    The plan is to have a stored procedure that can be called and given all the information about a transaction (user, items, quantities), which it will then place into the appropriate tables and automatically fill in price/transaction number/etc.

--- Smalltownshipsscript.sql
+++ Smalltownshipsscript.sql
@@ -18,6 +18,89 @@ USE `smalltownships`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `ims`
+--
+
+DROP TABLE IF EXISTS `ims`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ims` (
+  `transID` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  KEY `items_idx` (`itemID`),
+  KEY `trans_idx` (`transID`),
+  CONSTRAINT `items` FOREIGN KEY (`itemID`) REFERENCES `inventory` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `trans` FOREIGN KEY (`transID`) REFERENCES `transactions` (`transid`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ims`
+--
+
+LOCK TABLES `ims` WRITE;
+/*!40000 ALTER TABLE `ims` DISABLE KEYS */;
+INSERT INTO `ims` VALUES (10,2,1),(11,1,1),(11,3,1);
+/*!40000 ALTER TABLE `ims` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `unit_price` decimal(15,0) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES (1,'Iowa Class',5,1820000000),(2,'Deutschland Class',6,1550000000),(3,'Bismark Class',4,1699000000);
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `transid` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `price` decimal(15,0) NOT NULL,
+  `cardnumber` varchar(16) NOT NULL,
+  PRIMARY KEY (`transid`),
+  KEY `username_idx` (`username`),
+  CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `verifiedaccounts` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (10,'jSmit',1550000000,'0123456789010001'),(11,'jSmit',3519000000,'0123456789010001');
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `unverifiedaccounts`
 --
 
@@ -57,7 +140,9 @@ CREATE TABLE `verifiedaccounts` (
   `username` varchar(20) NOT NULL,
   `password` varchar(25) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `login` int(11) NOT NULL
+  `login` int(11) NOT NULL,
+  `permission` tinyint(2) unsigned zerofill DEFAULT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -67,7 +152,7 @@ CREATE TABLE `verifiedaccounts` (
 
 LOCK TABLES `verifiedaccounts` WRITE;
 /*!40000 ALTER TABLE `verifiedaccounts` DISABLE KEYS */;
-INSERT INTO `verifiedaccounts` VALUES ('John','Smith','jSmit','password','JSmith@email.com',0),('jane','doe','jDoe','mypass','jDoeADear@email.com',0),('jane','doe','jDoe','mypass','jDoeADear@email.com',0);
+INSERT INTO `verifiedaccounts` VALUES ('jane','doe','jDoe','mypass','jDoeADear@email.com',0,00),('John','Smith','jSmit','password','JSmith@email.com',1,01);
 /*!40000 ALTER TABLE `verifiedaccounts` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -80,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-17 16:18:51
+-- Dump completed on 2019-10-15 18:55:42
