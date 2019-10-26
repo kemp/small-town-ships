42014fa3c85df128eb45e507a998e1948c488d96
glend <glend@DESKTOP-4CTOL1U>
2019-10-26 10:36:58 -0400

    Updated SQL script to create database if it doesn't exist.

--- Smalltownshipsscript.sql
+++ Smalltownshipsscript.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `smalltownships` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `smalltownships`;
 -- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: smalltownships
