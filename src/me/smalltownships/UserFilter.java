374244de0ad9df4e0a2ccd5abe935ec91ae8a22a
Noah Huck <nhuck20@gmail.com>
2019-11-19 14:54:04 -0500

    UserFilter.java must implement inherited method destroy()

--- src/me/smalltownships/UserFilter.java
+++ src/me/smalltownships/UserFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -46,6 +47,18 @@ public class UserFilter implements Filter {
 		
 		/* Forward to the next request */
 		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
